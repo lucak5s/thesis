02d874b6a6ceae5d10043e72be3de23e6a1338fd
lucakempkes <luca.kempkes@rwth-aachen.de>
2024-02-20 04:26:52 +0100

    scheduling matroid

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-from matroids.partition_matroid import PartitionMatroid
+from matroids.scheduling_matroid import SchedulingMatroid
 from auction.bidder import Bidder
 from auction.unit_step_auction import unit_step_auction
 
-groundset = frozenset({1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-partitions = [
-  (frozenset({1, 2, 3, 4}), 2),
-  (frozenset({5, 6, 7}), 2),
-  (frozenset({8, 9, 10}), 2)
-]
-matroid = PartitionMatroid(groundset, partitions)
+deadlines = { 'g': 5, 'h': 5, 'i': 5, 'f': 3, 'e': 3, 'c': 2, 'd': 2, 'a': 1, 'b': 1 }
 
-bidder_a = Bidder({1: 2}, 'a')
-bidder_b = Bidder({2: 1}, 'b')
-bidder_c = Bidder({3: 24}, 'c')
-bidder_d = Bidder({4: 6}, 'd')
-bidder_e = Bidder({5: 2}, 'e')
-bidder_f = Bidder({6: 6}, 'f')
-bidder_g = Bidder({7: 3}, 'g')
-bidder_h = Bidder({8: 35}, 'h')
-bidder_i = Bidder({9: 61}, 'i')
-bidder_j = Bidder({10: 11}, 'j')
+matroid = SchedulingMatroid(deadlines, 1)
+
+bidder_a = Bidder({'a': 2}, 'a')
+bidder_b = Bidder({'b': 1}, 'b')
+bidder_c = Bidder({'c': 24}, 'c')
+bidder_d = Bidder({'d': 6}, 'd')
+bidder_e = Bidder({'e': 2}, 'e')
+bidder_f = Bidder({'f': 6}, 'f')
+bidder_g = Bidder({'g': 30}, 'g')
+bidder_h = Bidder({'h': 35}, 'h')
 
 
-base = unit_step_auction(matroid, [bidder_a, bidder_b, bidder_c, bidder_d, bidder_e, bidder_f, bidder_g, bidder_h, bidder_i, bidder_j])
+base = unit_step_auction(matroid, [bidder_a, bidder_b, bidder_c, bidder_d, bidder_e, bidder_f, bidder_g, bidder_h])
 print(base)
--- matroids/scheduling_matroid.py
+++ matroids/scheduling_matroid.py
@@ -1,18 +1,68 @@
 from matroids.matroid import Matroid
 
-class Uniform(Matroid):
-  def __init__(self, ):
-
-
+class SchedulingMatroid(Matroid):
+  def __init__(self, job_with_deadlines: dict, job_length: int):
+    self.deadlines = job_with_deadlines
+    self.dual_deadlines = get_dual_deadlines(job_with_deadlines)
+    self.job_length = job_length
+    
   def full_rank(self) -> int:
-
+    sorted_jobs = sorted(self.deadlines, key= lambda job: self.deadlines[job])
+    rank = 0
+    current_time = 0
+    for job in sorted_jobs:
+      job_start = self.deadlines[job] - self.job_length
+      if job_start >= current_time:
+        rank += 1
+        current_time = job_start + self.job_length
+    return rank
 
   def unique_cocircuit(self, X: frozenset) -> frozenset:
-
+    sorted_jobs = sorted(X, key= lambda job: self.dual_deadlines[job])
+    cocircuit = frozenset()
+    current_time = 0
+    for job in sorted_jobs:
+      job_start = self.dual_deadlines[job] - self.job_length
+      cocircuit = cocircuit.union(frozenset({job}))
+      if job_start < current_time: return cocircuit
+      current_time = job_start + self.job_length
+    return frozenset()
   
   def contract(self, element):
-
+    for job, deadline in self.deadlines.items():
+      if deadline >= self.deadlines[element]:
+        self.deadlines[job] -= 1
+    self.deadlines.pop(element)
+    self.dual_deadlines.pop(element)
 
   def delete(self, element):
+    for job, deadline in self.dual_deadlines.items():
+      if deadline >= self.dual_deadlines[element]:
+        self.dual_deadlines[job] -= 1
+    self.deadlines.pop(element)
+    self.dual_deadlines.pop(element)
 
 
+def get_dual_deadlines(deadlines: dict) -> dict:
+  sorted_jobs = sorted(deadlines, key= lambda job: deadlines[job], reverse=True)
+
+  curr_deadline = deadlines[sorted_jobs[0]]
+  jobs_with_curr_deadline = set(sorted_jobs[0])
+  offset = 0
+  dual_deadlines = {}
+
+  for job in sorted_jobs:
+    if deadlines[job] != curr_deadline:
+      offset += len(jobs_with_curr_deadline) - (curr_deadline - deadlines[job])
+      for j in jobs_with_curr_deadline:
+        dual_deadlines[j] = offset
+      jobs_with_curr_deadline.clear()
+      curr_deadline = deadlines[job]
+    jobs_with_curr_deadline.add(job)
+
+  if jobs_with_curr_deadline:
+    offset += len(jobs_with_curr_deadline) - (curr_deadline)
+    for j in jobs_with_curr_deadline:
+      dual_deadlines[j] = offset
+  
+  return dual_deadlines
