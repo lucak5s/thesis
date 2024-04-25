4804e084843cfd72ea4f96bd286894b84bec5f8e
lucakempkes <luca.kempkes@rwth-aachen.de>
2024-04-25 14:36:14 +0200

    testing

--- auction/tests/gammoid_auction_test.py
+++ auction/tests/gammoid_auction_test.py
@@ -22,19 +22,48 @@ def test_uniform_matroid():
 
   matroid = Gammoid(vertices, edges, starting_vertices, destination_vertices)
 
-  bidder_a = Bidder({'a': 12}, 'a')
-  bidder_b = Bidder({'b': 25}, 'b')
-  bidder_c = Bidder({'c': 24}, 'c')
-  bidder_d = Bidder({'d': 11}, 'd')
-  bidder_e = Bidder({'e': 33}, 'e')
-  bidder_f = Bidder({'f': 1}, 'f')
+  bidder_a = Bidder({'a': 12, 'b': 25}, 'a')
+  bidder_b = Bidder({'c': 24}, 'b')
+  bidder_c = Bidder({'d': 11, 'e': 33}, 'c')
+  bidder_d = Bidder({'f': 1}, 'd')
 
-  bidders = [bidder_a, bidder_b, bidder_c, bidder_d, bidder_e, bidder_f]
+  bidders = [bidder_a, bidder_b, bidder_c, bidder_d]
 
   base = unit_step_auction(matroid, bidders)
 
   assert base == frozenset({'b', 'c', 'e'})
 
 
+def test_large_uniform_matroid():
+  vertices = frozenset({
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+  })
+
+  
+  starting_vertices = frozenset({
+     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q'
+  })
+  destination_vertices = frozenset({
+    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+  })
+  
+  edges = []
+  for u in starting_vertices:
+    for v in destination_vertices:
+      edges.append((u, v))
+
+  matroid = Gammoid(vertices, edges, starting_vertices, destination_vertices)
+
+  bidder_a = Bidder({'a': 12, 'b': 25, 'c': 24}, 'a')
+  bidder_b = Bidder({'d': 13, 'e': 33}, 'b')
+  bidder_c = Bidder({'f': 11, 'g': 15, 'h': 33, 'i': 88}, 'c')
+  bidder_d = Bidder({'j': 6, 'k': 33, 'l': 22, 'm': 18}, 'd')
+  bidder_e = Bidder({'n': 63, 'o': 21, 'p': 43, 'q': 13}, 'e')
+
+  bidders = [bidder_a, bidder_b, bidder_c, bidder_d, bidder_e]
+
+  base = unit_step_auction(matroid, bidders)
+  
+  assert base == frozenset({'b', 'c', 'e', 'h', 'i', 'k', 'n', 'p', 'l'})
 
 
