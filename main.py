e16c84efca74ad3b7030005d65f2d08b4561f72e
lucakempkes <luca.kempkes@rwth-aachen.de>
2024-04-27 19:03:59 +0200

    improved plots

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from runtime_comparison.planar_graphic_linear import planar_graphic_linear_comparison
 from runtime_comparison.uniform_partition_gammoid_linear import uniform_partition_gammoid_linear_comparison
 import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 16})
 
 ### Planar - Graphic - Linear ###
 
@@ -10,11 +11,10 @@ density_types = ['sparse', 'semi-dense', 'dense']
 for density in density_types:
     runtimes = planar_graphic_linear_comparison(amounts_of_nodes, density_type=density)
     
-    plt.plot(amounts_of_nodes, runtimes['planar_runtimes'], label='Planar Matroid')
-    plt.plot(amounts_of_nodes, runtimes['graphic_runtimes'], label='Graphic Matroid')
-    plt.plot(amounts_of_nodes, runtimes['linear_runtimes'], label='Linear Matroid')
+    plt.plot(amounts_of_nodes, runtimes['planar_runtimes'], label='Planar Matroid', color='black', linewidth=2)
+    plt.plot(amounts_of_nodes, runtimes['graphic_runtimes'], label='Graphic Matroid', color='green', linewidth=2)
+    plt.plot(amounts_of_nodes, runtimes['linear_runtimes'], label='Linear Matroid', color='blue', linewidth=2)
     
-    plt.title(f'{density.capitalize()} Graph')
     plt.xlabel('Number of Nodes')
     plt.ylabel('Runtime (s)')
     plt.legend()
@@ -28,15 +28,13 @@ k_ratio = ['low-k', 'mid-k', 'high-k']
 for ratio in k_ratio:
   runtimes = uniform_partition_gammoid_linear_comparison(groundset_sizes, k_ratio)
 
-  plt.plot(groundset_sizes, runtimes['uniform_runtimes'], label='Uniform Matroid')
-  plt.plot(groundset_sizes, runtimes['partition_runtimes'], label='Partition Matroid')
-  plt.plot(groundset_sizes, runtimes['gammoid_runtimes'], label='Gammoid')
-  plt.plot([10, 12, 14, 16, 18, 20, 22, 24], runtimes['linear_runtimes'], label='Linear Matroid')
+  plt.plot(groundset_sizes, runtimes['uniform_runtimes'], label='Uniform Matroid', color='red', linewidth=3)
+  plt.plot(groundset_sizes, runtimes['partition_runtimes'], label='Partition Matroid', linestyle='--', color='black', linewidth=2)
+  plt.plot(groundset_sizes, runtimes['gammoid_runtimes'], label='Gammoid', color='green', linewidth=2)
+  plt.plot([10, 12, 14, 16, 18, 20, 22, 24], runtimes['linear_runtimes'], label='Linear Matroid', color='blue', linewidth=2)
 
-  plt.title(f"{ratio.capitalize()} Ratio")
   plt.xlabel('Size of Ground Set')
   plt.ylabel('Runtime (s)')
-  plt.yscale('log')
 
   plt.legend()
   plt.show()
